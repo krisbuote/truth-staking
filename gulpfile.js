30cd380cd80563421600582273372680de2cac02
Kris Buote <krisbuote@gmail.com>
2018-12-13 20:40:52 -0700

    html edits

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// Gulp.js configuration
+var
+  // modules
+  gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
+  htmlclean = require('gulp-htmlclean'),
+
+
+  // development mode?
+  devBuild = (process.env.NODE_ENV !== 'production'),
+
+  // folders
+  folder = {
+    src: 'src/',
+    build: 'build/'
+  }
+;
+
+// image processing
+gulp.task('images', function() {
+  var out = folder.build + 'images/';
+  return gulp.src(folder.src + 'images/**/*')
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(out));
+});
+
+// HTML processing
+gulp.task('html', ['images'], function() {
+  var out = folder.build + 'html/',
+    page = gulp.src(folder.src + 'html/**/*')
+      .pipe(newer(out));
+
+  // minify production code
+  if (!devBuild) {
+    page = page.pipe(htmlclean());
+  }
+
+  return page.pipe(gulp.dest(out));
+});
