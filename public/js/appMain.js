300667a25c5c60ab1858075215f8b78f25685320
Kris Buote <krisbuote@gmail.com>
2019-01-02 14:44:51 -0700

    modals on successful stakes

--- public/js/appMain.js
+++ public/js/appMain.js
@@ -318,6 +318,24 @@ App = {
 												${stakeButtonHTML}
 											</div>
 										</div>
+
+										<div class="modal fade text-dark" tabindex="-1" role="dialog" id="stakeSubmitModal${statementID}">
+										  <div class="modal-dialog" role="document">
+										    <div class="modal-content">
+										      <div class="modal-header">
+										        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+										        <h4 class="modal-title">Success!</h4>
+										      </div>
+										      <div id="stakeSubmitModal${statementID}Body" class="modal-body">
+										      </div>
+										      <div class="modal-footer">
+										        <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+										      </div>
+										    </div><!-- /.modal-content -->
+										  </div><!-- /.modal-dialog -->
+										</div><!-- /.modal -->
+
+										
 									</div>
 
 					            </form>
